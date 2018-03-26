8250f4e74bf2cd7bfbe43c5b0a2438e142e6c73d
ahs <ahs@localhost.localdomain>
2018-03-26 02:32:12 -0300

    Update bot Twitter

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
+// FUNCTION TO SEARCH TWEETS
 var params = {
   q: 'concurso publico espirito santo since:2016-03-26',
   count: 100
@@ -17,4 +18,15 @@ function gotData(err, data, response) {
   for (var i=0; i< tweets.length; i++) {
     console.log(tweets[i].text);
   }
+}
+
+// FUNCTION TO POST TWEETS
+var tweet = {
+  status: '#codificando campanha por node.js. Tutorials example in repository: https://github.com/ahsouza/botTwitter'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+  console.log(data)
 }
