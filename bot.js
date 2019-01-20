bbdaf140502dc4610a002d76bd38c85a32b1480f
ahsouza <annibalhsouza@gmail.com>
2019-01-20 00:31:13 -0200

    Updating somethings

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-console.log('o bot está iniciando');
+console.log('o bot está iniciando')
 
-var Twit = require('twit');
+var Twit = require('twit')
 
-var config = require('./config');
-var T = new Twit(config);
+var config = require('./config')
+var T = new Twit(config)
 
 // FUNCTION TO SEARCH TWEETS
 var params = {
@@ -11,12 +11,12 @@ var params = {
   count: 100
 }
 
-T.get('search/tweets', params, gotData);
+T.get('search/tweets', params, gotData)
 
 function gotData(err, data, response) {
-  var tweets = data.statuses;
+  var tweets = data.statuses
   for (var i=0; i< tweets.length; i++) {
-    console.log(tweets[i].text);
+    console.log(tweets[i].text)
   }
 }
 
@@ -25,7 +25,7 @@ var tweet = {
   status: '#publicando campanha no twitter com node.js. Exemplo em repositório: https://github.com/ahsouza/botTwitter'
 }
 
-T.post('statuses/update', tweet, tweeted);
+T.post('statuses/update', tweet, tweeted)
 
 function tweeted(err, data, response) {
   console.log(data)
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = proccess.env.PORT || 3000
  
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
+  console.log('Para derrubar o servidor: ctrl + c')
 })
