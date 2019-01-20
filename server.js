37d559f7e6cfae7378656df423d5d63d9404e70a
ahsouza <annibalhsouza@gmail.com>
2019-01-20 00:38:33 -0200

    Altering somethings

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = proccess.env.PORT || 3000
+const express = require('express'),
+ app = express(),
+ port = proccess.env.PORT || 3000
  
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
